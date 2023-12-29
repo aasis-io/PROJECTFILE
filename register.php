1cc3c277c987184329ef7e13a7f45cf96155ac3d
Ashish Thapa <heyashishthapa@gmail.com>
2023-12-29 22:40:57 +0545

    I guess all the validation part is done for the registration page

--- register.php
+++ register.php
@@ -10,44 +10,80 @@ $pattern = '/^(98[4-9]|97[7-9]|96[6-9])\d{7}$/';
 
 @session_start();
 if (isset($_POST['submit'])) {
-    $user->set('fullname', $_POST['fullname']);
 
+    $emptyName = $emptyAge = $emptyEmail = $emptyPhone = $emptyGender = $emptyOccupation = $emptyArea = $emptyAddress = $emptyPassword = $invalidEmail = $invalidPhone = $invalidPasswordLength = $invalidPassword = '';
 
     if (empty($_POST['fullname'])) {
         $emptyName = "Name Field Empty!";
     }
 
-    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    if (empty($_POST['age'])) {
+        $emptyAge = "Age Field Empty!";
+    }
+
+    if (empty($_POST['email'])) {
+        $emptyEmail = "Email Field Empty!";
+    } elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
         $invalidEmail = "Invalid Email Format!";
-    } elseif (strlen($_POST['phone']) != 10) {
-        $invalidPhone = "Invalid Phone Number Length!";
-    } elseif (!preg_match($pattern, $_POST['phone'])) {
-        $invalidPhone = "Invalid Number Format";
+    }
+
+    if (empty($_POST['phone'])) {
+        $emptyPhone = "Phone Field Empty!";
+    } elseif (strlen($_POST['phone']) != 10 || !preg_match($pattern, $_POST['phone'])) {
+        $invalidPhone = "Invalid Phone Number!";
+    }
+
+    if (empty($_POST['gender'])) {
+        $emptyGender = "Gender Not Selected!";
+    }
+
+    if (empty($_POST['occupation'])) {
+        $emptyOccupation = "Occupation Not Selected!";
+    }
+
+    if (empty($_POST['area'])) {
+        $emptyArea = "Area Not Selected!";
+    }
+
+    if (empty($_POST['address'])) {
+        $emptyAddress = "Address Field Empty!";
+    }
+
+    if (empty($_POST['confirmPassword'])) {
+        $emptyCPassword = "Field is Empty!";
+    }
+
+    if (empty($_POST['password'])) {
+        $emptyPassword = "Password Field Empty!";
     } elseif (strlen($_POST['password']) < 8) {
         $invalidPasswordLength = "Minimum Password Length is 8!";
-    } elseif (($_POST['password']) != ($_POST['confirmPassword'])) {
+    } elseif ($_POST['password'] != $_POST['confirmPassword']) {
         $invalidPassword = "Password Doesn't Match!";
-    } else {
+    }
+
+    if (empty($emptyName) && empty($emptyAge) && empty($emptyEmail)
+     && empty($emptyPhone) && empty($emptyGender)
+     && empty($emptyOccupation) && empty($emptyArea)
+      && empty($emptyAddress) && empty($emptyPassword) 
+      && empty($invalidEmail) && empty($invalidPhone)
+       && empty($invalidPasswordLength) && empty($invalidPassword)) {
+
+        $user->set('fullname', $_POST['fullname']);
         $user->set('email', $_POST['email']);
         $user->set('age', $_POST['age']);
         $user->set('phone', $_POST['phone']);
         $user->set('gender', $_POST['gender']);
-        $user->set('occupation', $_POST['fullname']);
+        $user->set('occupation', $_POST['occupation']);
         $user->set('area', $_POST['area']);
         $user->set('address', $_POST['address']);
         $user->set('password', $_POST['password']);
 
         $user->save();
-        // if (is_integer($result)) {
-        //     $ErrMs = "";
-        //     $msg = "Registered Successfully with id " . $result;
-        // } else {
-        //     $msg = "";
-        // }
+        // Additional handling after saving the user, if needed
     }
 }
-
 ?>
+
 
 
 
@@ -96,11 +132,21 @@ if (isset($_POST['submit'])) {
                             <div class="input-field">
                                 <label>Full Name</label>
                                 <input type="text" placeholder="Enter your name" name="fullname" required>
+                                <?php if(isset($emptyName)){ ?>
+                                    
+                                    <small>  <?php echo $emptyName; ?>  </small>
+                                    
+                                    <?php } ?>
                             </div>
 
                             <div class="input-field">
                                 <label>Age</label>
                                 <input type="number" placeholder="Enter your age" name="age" required>
+                                <?php if(isset($emptyAge)){ ?>
+                                    
+                                    <small>  <?php echo $emptyAge; ?>  </small>
+                                    
+                                    <?php } ?>
                             </div>
 
                             <div class="input-field">
@@ -109,6 +155,9 @@ if (isset($_POST['submit'])) {
                                 <?php if (isset($invalidEmail)) { ?>
                                     <small> <?php echo $invalidEmail ?></small>
                                 <?php } ?>
+                                <?php if (isset($emptyEmail)) { ?>
+                                    <small> <?php echo $emptyEmail ?></small>
+                                <?php } ?>
                             </div>
 
                             <div class="input-field">
@@ -116,6 +165,10 @@ if (isset($_POST['submit'])) {
                                 <input type="number" placeholder="Enter mobile number" name="phone" required>
                                 <?php if (isset($invalidPhone)) { ?>
                                     <small> <?php echo $invalidPhone ?></small>
+                                <?php } ?>
+
+                                <?php if (isset($emptyPhone)) { ?>
+                                    <small> <?php echo $emptyPhone ?></small>
                                 <?php } ?>
 
                             </div>
@@ -127,6 +180,9 @@ if (isset($_POST['submit'])) {
                                     <option>Male</option>
                                     <option>Female</option>
                                 </select>
+                                <?php if (isset($emptyGender)) { ?>
+                                    <small> <?php echo $emptyGender ?></small>
+                                <?php } ?>
                             </div>
 
                             <div class="input-field">
@@ -137,8 +193,10 @@ if (isset($_POST['submit'])) {
                                     <option>Electrician</option>
                                     <option>Painter</option>
                                     <option>Carpenter</option>
-
                                 </select>
+                                <?php if (isset($emptyOccupation)) {?>
+                                    <small> <?php echo $emptyOccupation?></small>
+                                <?php }?>
                             </div>
                         </div>
                     </div>
@@ -156,11 +214,18 @@ if (isset($_POST['submit'])) {
                                     <option>Gurujudhara</option>
                                     <option>Bafal</option>
                                 </select>
+                                <?php if (isset($emptyArea)) {?>
+                                    <small> <?php echo $emptyArea?></small>
+                                <?php }?>
                             </div>
 
                             <div class="input-field">
                                 <label>Full Address</label>
                                 <input type="text" placeholder="Enter your address" name="address" required>
+                                <?php if (isset($emptyAddress)) {?>
+                                    <small> <?php echo $emptyAddress?></small>
+                                <?php }?>
+                                
                             </div>
                         </div>
 
@@ -173,7 +238,9 @@ if (isset($_POST['submit'])) {
                             <div class="input-field">
                                 <label>Create Password</label>
                                 <input type="password" id="passwordField" name="password" placeholder="Create new password" required>
-
+                                <?php if (isset($emptyPassword)) { ?>
+                                    <small> <?php echo $emptyPassword ?></small>
+                                <?php } ?>
                             </div>
 
                             <div class="input-field">
@@ -182,6 +249,10 @@ if (isset($_POST['submit'])) {
                                 <?php if (isset($invalidPassword)) { ?>
                                     <small> <?php echo $invalidPassword ?></small>
                                 <?php } ?>
+
+                                <?php if (isset($emptyCPassword)) {?>
+                                    <small> <?php echo $emptyCPassword?></small>
+                                <?php }?>
                             </div>
                             <div class="input-field show-password">
                                 <input type="checkbox" id="passwordToggler" onclick="togglePassword()"> <label for="passwordToggler">Show
